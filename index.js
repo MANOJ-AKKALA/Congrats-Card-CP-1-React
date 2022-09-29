952ab81d6cadf25d8707a80f430fec58e6cebb10
AkkalaManoj <amanojkg@gmail.com>
2022-09-29 03:36:40 +0000

    congrats card cp-1-React

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  // Write your code here.
+
+  <div className="bg-1">
+    <h1 className="heading">Congratulations</h1>
+    <div className="bg-2 card-size">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+      />
+
+      <h1>Kiran V</h1>
+
+      <p>Vishnu Institute of Computer Education and Technology. Bhimavaram.</p>
+
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
